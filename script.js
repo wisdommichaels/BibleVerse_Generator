a51efc3bea1c8a510f7e70ec70d0a13e6de8117e
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-14 13:12:40 +0100

    addedjs functionality

---
+++ script.js
@@ -0,0 +1,22 @@
+const background = document.getElementById("#background")
+const bibleChapter = document.getElementById("book-chapter")
+const verse = document.getElementById("verse")
+const btn = document.getElementById("btn")
+
+
+let Quotes = [
+    {
+        bibleChapter: "1 Corinthians 13:4-7",
+        verse: "Love is patient, love is kind. It does not envy, it does not boast, it is not proud. It does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs. Love does not delight in evil but rejoices with the truth. It always protects, always trusts, always hopes, always perseveres."
+    },
+    {
+        bibleChapter: "2 Timothy 1:7",
+        verse: "For the Spirit God gave us does not make us timid, but gives us power, love and self-discipline."
+    }
+
+]
+
+btn.addEventListener("click"()) => function  bookChapter() {
+    const
+    
+}
